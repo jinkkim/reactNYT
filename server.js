22c90b5b69e61b8aaa5ba38753e441b91ff2ec76
Jin <celine.jkim@gmail.com>
2018-07-06 23:09:47 -0400

    mongoDB added to heroku

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.static(process.cwd() + '/public'));
 //mongoDB
 var mongoose = require("mongoose");
 mongoose.Promise = Promise;
-var mongoDBUrl = "mongodb://localhost:27017/nytreact";
-//var mongoDBUrl = -------- heroku --------
+//var mongoDBUrl = "mongodb://localhost:27017/nytreact";
+var mongoDBUrl = "mongodb://heroku_jkqw4m46:khrgvngfl5mvc491a859au4uc8@ds129811.mlab.com:29811/heroku_jkqw4m46"
 mongoose.connect(mongoDBUrl, function(error)
 	{
 	console.log("Database connected");
